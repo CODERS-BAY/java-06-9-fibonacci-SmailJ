963006ea0d8a6d625a97820d3d0d327018bfdf3a
Smail <smail.jusic16@gmail.com>
2020-09-14 14:56:02 +0200

    Ready

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -1,9 +1,25 @@
 package com.codersbay;
 
+import java.util.Scanner;
+
 public class Fibonacci {
 
     public static void main(String[] args) {
-        System.out.println("Hello Fibonacci");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Which number you want to now the fibionnaci thing ?");
+        int zahl = s.nextInt();
+
+        int zahl1 = 0;
+        int zahl2 = 1;
+        int zahl3;
+
+        for (int i = 1; i < zahl; ++i) {
+            zahl3 = zahl1 + zahl2;
+            zahl1 = zahl2;
+            zahl2 = zahl3;
+        }
+
+        System.out.println(zahl2);
     }
 
 }
