74ccf403788661525e55aa204e0ecf18fab53f4f
Smail <smail.jusic16@gmail.com>
2020-09-14 15:26:44 +0200

    Ready

--- src/com/codersbay/Fibonacci.java
+++ src/com/codersbay/Fibonacci.java
@@ -5,21 +5,34 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("Which number you want to now the fibionnaci thing ?");
-        int zahl = s.nextInt();
+        /*Scanner sc = new Scanner(System.in);
+        System.out.println("Which number you want to know the fibionnaci thing ?");
+        int number = sc.nextInt();
 
-        int zahl1 = 0;
-        int zahl2 = 1;
-        int zahl3;
+        int number1 = 0;
+        int number2 = 1;
+        int number3;
 
-        for (int i = 1; i < zahl; ++i) {
-            zahl3 = zahl1 + zahl2;
-            zahl1 = zahl2;
-            zahl2 = zahl3;
+        for (int i = 1; i < number; ++i) {
+            number3 = number1 + number2;
+            number1 = number2;
+            number2 = number3;
         }
 
-        System.out.println(zahl2);
+        System.out.println(number2);*/
+        System.out.println("Which fibonacci number do you search ?");
+        Scanner sc = new Scanner(System.in);
+        int fibo = sc.nextInt();
+        System.out.println(fibonacciFormula(fibo));
+    }
+
+    private static int fibonacciFormula(int number) {
+
+        if (number == 0 || number == 1) {
+            return number;
+        } else {
+            return fibonacciFormula(number - 1) + fibonacciFormula(number - 2);
+        }
     }
 
 }
